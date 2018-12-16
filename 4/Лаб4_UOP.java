5d1173406915e8d5773a69e7aa4a943c0c49251a
rackpack <32074731+rackpack@users.noreply.github.com>
2018-12-16 12:45:18 +0300

    Update Лаб4_UOP.java

--- 4/Лаб4_UOP.java
+++ 4/Лаб4_UOP.java
@@ -1,1 +1,17 @@
 
+package ru.mirea.leo;
+
+public class UOP
+{
+    public static void main(String[] args) {
+        Computer comp = new Computer(1000);
+        Car car = new Car(100000);
+        Horse horse = new Horse(20000);
+        int compp = comp.getPrice();
+        int carp = car.getPrice();
+        int horsep = horse.getPrice();
+        System.out.println("Цена компьютера: $" + String.valueOf(compp));
+        System.out.println("Цена машины: $" + String.valueOf(carp));
+        System.out.println("Цена лошади: $" + String.valueOf(horsep));
+    }
+}
