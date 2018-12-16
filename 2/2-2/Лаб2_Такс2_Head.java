5dc7a94a364d46aae51478b43f8cefd8839841f9
rackpack <32074731+rackpack@users.noreply.github.com>
2018-12-16 12:38:13 +0300

    Create Лаб2_Такс2_Head.java

---
+++ 2/2-2/Лаб2_Такс2_Head.java
@@ -0,0 +1,14 @@
+package myhuman;
+
+public class Head
+{
+    private int length;
+    public int getLength()
+    {
+        return length;
+    }
+    public void setLength(int l)
+    {
+        length = l;
+    }
+}
