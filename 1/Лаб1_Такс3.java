05715d729cc3188e2ec1c071dccc7af8d2dea462
rackpack <32074731+rackpack@users.noreply.github.com>
2018-12-16 12:33:25 +0300

    Create Лаб1_Такс3.java

---
+++ 1/Лаб1_Такс3.java
@@ -0,0 +1,15 @@
+package harmonic;
+
+//Lab 1, task 3
+
+public class OutputH {
+    public static void main(String[] args)
+    {
+        System.out.println("Гармоническая последовательность");
+        float i = 1;
+        while (i < 10) {
+            System.out.println(1 / i);
+            i++;
+        }
+    }
+}
