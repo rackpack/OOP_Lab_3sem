075134569ec76aca586d3e50269226306fe023f7
rackpack <32074731+rackpack@users.noreply.github.com>
2018-12-16 12:39:05 +0300

    Create Лаб2_Таск2_Hand.java

---
+++ 2/2-2/Лаб2_Таск2_Hand.java
@@ -0,0 +1,14 @@
+package myhuman;
+
+public class Hand
+{
+    private int length;
+    public int getLength()
+    {
+        return length;
+    }
+    public void setLength(int l)
+    {
+        length = l;
+    }
+}
